3cc01526d029998c68edd928139ce72b3bcbaeed
Tony <tonymike99@gmail.com>
2022-05-23 22:25:50 +0530

    added Trash page

---
+++ src/pages/Trash/Trash.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { Aside, CardNote } from "../../components/index";
+import { useDocumentTitle } from "../../hooks/custom/index";
+import { useTrash } from "../../hooks/context/index";
+
+function Trash() {
+  // SET DOCUMENT TITLE
+  useDocumentTitle("Trash");
+
+  // ****************************************************************************************************
+
+  const { trash, getTrash } = useTrash();
+
+  useEffect(() => {
+    getTrash();
+  }, []);
+
+  // ****************************************************************************************************
+
+  return (
+    <div className="main-container">
+      <Aside />
+
+      <main className="main">
+        <section className="notes">
+          {trash.length ? (
+            trash.map((note) => <CardNote key={note._id} data={note} />)
+          ) : (
+            <h1>Trash is empty! 😍</h1>
+          )}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export { Trash };
