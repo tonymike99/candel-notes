8254991e64003116234c95064f0efa116264f067
Tony <tonymike99@gmail.com>
2022-05-23 22:26:15 +0530

    added sort functionality

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,8 @@ function Home() {
 
   // ****************************************************************************************************
 
-  const { notes, getNotes } = useNotes();
+  const { notes, getNotes, setNotes } = useNotes();
+  let priorityLevels = { "": 0, Low: 1, Medium: 2, High: 3 };
 
   // ****************************************************************************************************
 
@@ -19,11 +20,101 @@ function Home() {
 
   // ****************************************************************************************************
 
+  const handlerSortByOldestToNewestDate = (e) => {
+    if (e.target.checked)
+      setNotes([
+        ...notes.sort(function (a, b) {
+          return new Date(a.date) - new Date(b.date);
+        }),
+      ]);
+  };
+
+  const handlerSortByNewestToOldestDate = (e) => {
+    if (e.target.checked)
+      setNotes([
+        ...notes.sort(function (a, b) {
+          return new Date(b.date) - new Date(a.date);
+        }),
+      ]);
+  };
+
+  const handlerSortByLowToHighPriority = (e) => {
+    if (e.target.checked)
+      setNotes([
+        ...notes.sort(function (a, b) {
+          return priorityLevels[a.priority] - priorityLevels[b.priority];
+        }),
+      ]);
+  };
+
+  const handlerSortByHighToLowPriority = (e) => {
+    if (e.target.checked)
+      setNotes([
+        ...notes.sort(function (a, b) {
+          return priorityLevels[b.priority] - priorityLevels[a.priority];
+        }),
+      ]);
+  };
+
+  // ****************************************************************************************************
+
   return (
     <div className="main-container">
       <Aside />
 
       <main className="main">
+        <section className="filters">
+          <div>
+            <h3>Sort by Date</h3>
+            <div>
+              <input
+                type="radio"
+                id="sortByOldestToNewestDate"
+                name="sortByDate"
+                onChange={(e) => handlerSortByOldestToNewestDate(e)}
+              />
+              <label htmlFor="sortByOldestToNewestDate">
+                {" "}
+                Oldest to Newest{" "}
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="sortByNewestToOldestDate"
+                name="sortByDate"
+                onChange={(e) => handlerSortByNewestToOldestDate(e)}
+              />
+              <label htmlFor="sortByNewestToOldestDate">
+                {" "}
+                Newest to Oldest{" "}
+              </label>
+            </div>
+          </div>
+
+          <div>
+            <h3>Sort by Priority</h3>
+            <div>
+              <input
+                type="radio"
+                id="sortByLowToHighPriority"
+                name="sortByPriority"
+                onChange={(e) => handlerSortByLowToHighPriority(e)}
+              />
+              <label htmlFor="sortByLowToHighPriority"> Low to High </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="sortByHighToLowPriority"
+                name="sortByPriority"
+                onChange={(e) => handlerSortByHighToLowPriority(e)}
+              />
+              <label htmlFor="sortByHighToLowPriority"> High to Low </label>
+            </div>
+          </div>
+        </section>
+
         <section className="notes">
           <CardMain />
           {notes.map((note) => (
