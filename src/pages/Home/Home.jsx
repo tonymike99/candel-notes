1184fef6c2d1bd960b15a252b976b81fcacb6266
Tony <tonymike99@gmail.com>
2022-06-26 18:22:26 +0530

    modified home page styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import { Aside } from "../../components/index";
 import { useDocumentTitle } from "../../hooks/custom/index";
-const girlTakingNotesPic = require("../../assets/images/Taking notes-bro.png");
+import { useAuth } from "../../hooks/context/index";
+const takingNotesImage = require("../../assets/images/taking_notes.svg");
 
 function Home() {
   // SET DOCUMENT TITLE
   useDocumentTitle("Home");
+
+  // ****************************************************************************************************
+
+  const { encodedToken } = useAuth();
 
   // ****************************************************************************************************
 
@@ -14,27 +19,32 @@ function Home() {
       <Aside />
 
       <main className="main">
-        <section className="home-content">
-          <div className="text-hero">
-            <h1>Meet your modern </h1>
-            <h1 className="h2">
+        <section className="hero-content">
+          <div className="hero-text">
+            <h3 className="h3">Meet your modern </h3>
+
+            <h2 className="h2">
               <span className="color-blue">Note Taking App</span>
-            </h1>
-            <small className="text-center">
+            </h2>
+
+            <p className="text-grey text-center">
               Manage your daily tasks and workflow in a modern way and boost
               your efficiency without any efforts.
-            </small>
-            <Link to="/auth">
-              <button className="btn btn-primary btn-width-extended pointer">
-                Log in
-              </button>
-            </Link>
+            </p>
+
+            {!encodedToken && (
+              <Link to="/auth">
+                <button className="btn btn-primary btn-fixed pointer">
+                  Log in
+                </button>
+              </Link>
+            )}
           </div>
 
-          <div className="image-hero">
+          <div className="hero-image">
             <img
               className="image-responsive"
-              src={girlTakingNotesPic.default}
+              src={takingNotesImage.default}
               alt=""
             />
           </div>
